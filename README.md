c48d32ddb36c8898e74834610d72edf548dc3f38
David Lee <daviddwlee84@gmail.com>
2018-05-10 20:59:33 +0800

    Add the tests

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# PythonUnitTesting
-Practice continuous integration of python with Travis CI
+# Python Unit Testing
+
+## Links
+
+[Travis CI](https://travis-ci.org)
+
+## Notes
+
+### Unit testing
+
+* We only want our test to fail if something is wrong with "our code".
+
+### Python unit testing framework
+
+* unittest - standard library
+* nose
+* pytest <- best
+
+### Python import from different folder
+
+* Set path
+
+```python
+import sys
+sys.path.append('./path/to/file/folder')
+```
+
+* Get current path
+
+```python
+import os
+os.getcwd()
+```
+
+[Python 獲取文件路徑及文件目錄(\_\_file\_\_ 的使用方法)](https://github.com/dokelung/Python-QA/blob/master/questions/standard_lib/Python%20獲取文件路徑及文件目錄(__file__%20的使用方法).md)
+
+
+### Test command
+
+> -v flag => detail
+
+* General: `py.test`
+
+* pytest
+    * `python3 -m pytest [file.py]`
+
+* unittest
+    * `python3 -m unittest [file.py]`
+
+## Resources
+
+### unittest
+
+[Python Tutorial: Unit Testing Your Code with the unittest Module](https://www.youtube.com/watch?v=6tNS--WetLI)
+
+### pytest
+
+[Pytest Tutorial Video List](https://www.youtube.com/playlist?list=PLeo1K3hjS3utzQYDNRNluzqJqpMXx6hHu)
+
+## Documents
+
+[`unittest`](https://docs.python.org/3/library/unittest.html#unittest.TestCase.debug)
+
+[`pytest`]
---
+++ unittest/test/test_employee.py
@@ -0,0 +1,77 @@
+import unittest
+from unittest.mock import patch
+import sys, os
+sys.path.append(os.getcwd() + '/unittest/src')
+from employee import Employee
+
+
+class TestEmployee(unittest.TestCase):
+
+    # one is before anything
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    # one is after everything
+    @classmethod
+    def tearDownClass(cls):
+        print('teardownClass')
+
+    # before each test
+    def setUp(self):
+        print('setUp')
+        self.emp_1 = Employee('Corey', 'Schafer', 50000)
+        self.emp_2 = Employee('Sue', 'Smith', 60000)
+
+    # after each test
+    def tearDown(self):
+        print('tearDown\n')
+
+    def test_email(self):
+        print('test_email')
+        self.assertEqual(self.emp_1.email, 'Corey.Schafer@email.com')
+        self.assertEqual(self.emp_2.email, 'Sue.Smith@email.com')
+
+        self.emp_1.first = 'John'
+        self.emp_2.first = 'Jane'
+
+        self.assertEqual(self.emp_1.email, 'John.Schafer@email.com')
+        self.assertEqual(self.emp_2.email, 'Jane.Smith@email.com')
+
+    def test_fullname(self):
+        print('test_fullname')
+        self.assertEqual(self.emp_1.fullname, 'Corey Schafer')
+        self.assertEqual(self.emp_2.fullname, 'Sue Smith')
+
+        self.emp_1.first = 'John'
+        self.emp_2.first = 'Jane'
+
+        self.assertEqual(self.emp_1.fullname, 'John Schafer')
+        self.assertEqual(self.emp_2.fullname, 'Jane Smith')
+
+    def test_apply_raise(self):
+        print('test_apply_raise')
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
+
+        self.assertEqual(self.emp_1.pay, 52500)
+        self.assertEqual(self.emp_2.pay, 63000)
+
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = 'Success'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/Schafer/May')
+            self.assertEqual(schedule, 'Success')
+
+            mocked_get.return_value.ok = False
+
+            schedule = self.emp_2.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/Smith/June')
+            self.assertEqual(schedule, 'Bad Response!')
+
+
+if __name__ == '__main__':
+    unittest.main()
